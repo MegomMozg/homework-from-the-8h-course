af7cf4ec6af7fac338d7d1e5d72fef761ebbc556
Markus <polayna05@mail.ru>
2022-05-23 18:02:29 +0300

    project

---
+++ CarGameMobile-master/Assets/_Root/Scripts/EntryPoint.cs
@@ -0,0 +1,28 @@
+using Game;
+using Profile;
+using UnityEngine;
+
+internal class EntryPoint : MonoBehaviour
+{
+    [Header("Initial Settings")]
+    [SerializeField] private float _speedCar;
+    [SerializeField] private GameState _initialState;
+    [SerializeField] private TransportType _transportType;
+
+    [Header("Scene Objects")]
+    [SerializeField] private Transform _placeForUi;
+
+    private MainController _mainController;
+
+
+    private void Awake()
+    {
+        var profilePlayer = new ProfilePlayer(_speedCar, _transportType, _initialState);
+        _mainController = new MainController(_placeForUi, profilePlayer);
+    }
+
+    private void OnDestroy()
+    {
+        _mainController.Dispose();
+    }
+}
