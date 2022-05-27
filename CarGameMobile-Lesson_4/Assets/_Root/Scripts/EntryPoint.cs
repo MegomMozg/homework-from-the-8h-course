73654efa52d5013fe683d7d6b81d335aba7f5103
Markus <polayna05@mail.ru>
2022-05-27 11:22:27 +0300

    Revert "проблемный со"
    This reverts commit 99c393f13f738a92f0dbd19fd00a2376d85eabd8.

--- CarGameMobile-Lesson_4/Assets/_Root/Scripts/EntryPoint.cs
+++ CarGameMobile-Lesson_4/Assets/_Root/Scripts/EntryPoint.cs
@@ -1,18 +1,9 @@
-<<<<<<< Updated upstream
 using Game;
 using Profile;
-=======
-using _Root.Scripts;
-using Game;
-using Profile;
-using Tool;
-using Unity.VisualScripting;
->>>>>>> Stashed changes
 using UnityEngine;
 
 internal class EntryPoint : MonoBehaviour
 {
-<<<<<<< Updated upstream
     [Header("Initial Settings")]
     [SerializeField] private float _speedCar;
     [SerializeField] private GameState _initialState;
@@ -27,17 +18,6 @@ internal class EntryPoint : MonoBehaviour
     private void Awake()
     {
         var profilePlayer = new ProfilePlayer(_speedCar, _transportType, _initialState);
-=======
-    [Header("Scene Objects")]
-    [SerializeField] private Transform _placeForUi;
-
-    private EntryPointSettings _settings;
-    private MainController _mainController;
-    private void Start()
-    {
-        _settings = Resources.Load<EntryPointSettings>("Configs/EntryPointSettings");
-        var profilePlayer = new ProfilePlayer(_settings._speedCar, _settings._transportType, _settings._initialState);
->>>>>>> Stashed changes
         _mainController = new MainController(_placeForUi, profilePlayer);
     }
 
