754d749675b914db56a15474ce1d4ecdc2451d24
Markus <polayna05@mail.ru>
2022-05-27 19:29:07 +0300

    первое задание

--- CarGameMobile-Lesson_4/Assets/_Root/Scripts/EntryPoint.cs
+++ CarGameMobile-Lesson_4/Assets/_Root/Scripts/EntryPoint.cs
@@ -1,23 +1,22 @@
+using _Root.Scripts;
 using Game;
 using Profile;
 using UnityEngine;
 
 internal class EntryPoint : MonoBehaviour
 {
-    [Header("Initial Settings")]
-    [SerializeField] private float _speedCar;
-    [SerializeField] private GameState _initialState;
-    [SerializeField] private TransportType _transportType;
-
+    
     [Header("Scene Objects")]
     [SerializeField] private Transform _placeForUi;
+
+    [SerializeField] private EntryPointSettings _settings;
 
     private MainController _mainController;
 
 
     private void Awake()
     {
-        var profilePlayer = new ProfilePlayer(_speedCar, _transportType, _initialState);
+        var profilePlayer = new ProfilePlayer(_settings._speedCar, _settings._transportType, _settings._initialState);
         _mainController = new MainController(_placeForUi, profilePlayer);
     }
 
