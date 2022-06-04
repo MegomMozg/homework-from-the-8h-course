73bd7f95a79d3da8c2b40f07e19bfb1d6a0fc6c8
Markus <polayna05@mail.ru>
2022-06-04 12:55:50 +0300

    дз

--- CarGameMobile-master/Assets/_Rewards/Scripts/InstallView.cs
+++ CarGameMobile-master/Assets/_Rewards/Scripts/InstallView.cs
@@ -5,17 +5,28 @@ namespace Rewards
     internal class InstallView : MonoBehaviour
     {
         [SerializeField] private DailyRewardView _dailyRewardView;
+        [SerializeField] private WeeklyRewardView _weeklyRewardView;
 
         private DailyRewardController _dailyRewardController;
+        private WeeklyRewardController _weeklyRewardController;
 
 
-        private void Awake() =>
+        private void Awake()
+        {
             _dailyRewardController = new DailyRewardController(_dailyRewardView);
+            _weeklyRewardController = new WeeklyRewardController(_weeklyRewardView);
+        }
 
-        private void Start() =>
+        private void Start()
+        {
+            _weeklyRewardController.Init();
             _dailyRewardController.Init();
+        }
 
-        private void OnDestroy() =>
+        private void OnDestroy()
+        {
             _dailyRewardController.Deinit();
+            _weeklyRewardController.Deinit();
+        }
     }
 }
